2c8d7f3b29e059d7c1b4fcb98767fc940b7f74ef
Axel Nuñez <aranuo23@gmail.com>
2024-08-20 23:31:25 -0300

    Agregando CartManager

--- proyecto_node_ecommerce/src/routes/carts.routes.js
+++ proyecto_node_ecommerce/src/routes/carts.routes.js
@@ -1,26 +1,10 @@
 import { Router } from "express";
 const router = Router();
 
-let carts = []
 
 //GET
 router.get("/", (req,res)=>{
-    res.send(carts);
-});
-
-//POST  
-router.post("/", (req,res)=>{
-    let cart = req.body;
-    console.log(cart);
-
-    if(!cart.producto){
-        return res.status(400).send({status:"error", msg:"valores incompletos"});
-    }
-
-    carts.push(cart);
-    console.log(cart);
-
-    return res.send({status:"succes", msg:"carrito creado"}); 
+    res.send("En desarrollo");
 });
 
 
