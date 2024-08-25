6f5bb90afa16cea5fe063c31e77f3153d841f095
Axel Nuñez <aranuo23@gmail.com>
2024-08-25 18:00:51 -0300

    por fin pude hacer que se agregue un id del producto a un carrito especifico

--- proyecto_node_ecommerce/src/routes/carts.routes.js
+++ proyecto_node_ecommerce/src/routes/carts.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";   
+// multer import { uploader } from "../utils.js";
 import CartManager from "../service/CartManager.js";
-import ProductManager from "../service/ProductManager.js";
 
 const router = Router();
 const cartManager = new CartManager();
@@ -15,12 +15,14 @@ router.get("/:cid", async (req,res)=>{
         if(cart) {
             res.json(cart)
         }else{
-            res.status(404).json({error: "Producto no encontrado"});
+            res.status(404).json({error: "Carrito no encontrado"});
         }
     } catch (error) {
         console.log(error)
     }
 });
+
+
 
 //POST
 router.post("/", async (req,res) => {
@@ -31,6 +33,25 @@ router.post("/", async (req,res) => {
         console.log(error);
     }
 })
+
+
+
+//POST
+router.post("/:cid/product/:pid", async (req,res) =>{
+    try {
+        const cartId = req.params.cid
+        const productId = req.params.pid;
+        const productToCart = await cartManager.addProductToCart(cartId,productId);
+            if (productToCart) {
+                res.json(cartManager.carts)
+            }else{
+                res.status(404).json({error: "No fue posible agregar al carrito"});
+            }
+    } catch (error) {
+        console.log(error);
+    }
+})
+
 
 
 export default router;
--- proyecto_node_ecommerce/src/service/CartManager.js
+++ proyecto_node_ecommerce/src/service/CartManager.js
@@ -2,8 +2,11 @@ import { json } from "express";
 import fs from "fs/promises";
 import path from "path";
 import { v4 as uuid } from "uuid";
+import ProductManager from "./ProductManager.js";
+import { error, log } from "console";
 
 const carritosFilePath = path.resolve("data", "carts.json");
+const productManagerToCarrito = new ProductManager();
 
 
 export default class CartManager{
@@ -43,7 +46,26 @@ export default class CartManager{
         return newCart;
     }   
 
+    addProductToCart(cartId, productId){
+        //para acceder a la propiedad product necesitamos encontrar el carrito especifico (lo cual le indicamos por id)
+        const cart = this.carts.find(cart => cart.id == cartId);
+        //console.log("Este es el id de mi carrito correcto: ",cart);
+        if (!cart){
+            throw new error("Carrito no encontrado")
+        }
 
+        //una vez que encontramos el carrito al cual acceder, colocamos el id del producto y cantidad inicializa en 1
+        const newProductToCart = {
+            product : productId,
+            quantity : 1
+        };
+        
+        //Accedemos al carrito especifico y luego al array product "cart.product" y pusheamos 
+        cart.product.push(newProductToCart);
+        
+        this.saveToFile();
+        return newProductToCart;
+    }
 
 
 
