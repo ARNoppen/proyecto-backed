b118e23254ec48890c0fe1e487ffdfdfdd06e09f
Axel_Núñez <131285439+ARNoppen@users.noreply.github.com>
2025-07-01 20:07:20 -0300

    Agrega rutas de productos y detalle

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,11 +2,15 @@ import { createRouter, createWebHistory } from 'vue-router';
 import Register from '../views/Register.vue';
 import Login from '../views/Login.vue';
 import ChangePassword from '../views/ChangePassword.vue';
+import Products from '../views/Products.vue';
+import ProductDetail from '../views/ProductDetail.vue';
 
 const routes = [
   { path: '/register', component: Register },
   { path: '/login', component: Login },
   { path: '/changepassword', component: ChangePassword},
+  { path: '/products', component: Products },
+  { path: '/products/:pid', component: ProductDetail },
 ];
 
 const router = createRouter({
